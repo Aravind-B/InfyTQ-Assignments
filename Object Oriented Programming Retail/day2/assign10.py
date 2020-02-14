6e218afcfe75c370ae9aa2ebb95da2aa13e557cd
Aravind-B <aravind.k.bist@gmail.com>
2020-02-14 21:08:17 +0530

    Add files via upload

---
+++ Object Oriented Programming Retail/day2/assign10.py
@@ -0,0 +1,27 @@
+#OOPR-Assgn-10
+
+class CallDetail:
+    def __init__(self,phoneno,called_no,duration,call_type):
+        self.__phoneno=phoneno
+        self.__called_no=called_no
+        self.__duration=duration
+        self.__call_type=call_type
+
+class Util:
+    def __init__(self):
+        self.list_of_call_objects=None
+
+    def parse_customer(self,list_of_call_string):
+        self.list_of_call_objects=[]
+        for i in list_of_call_string:
+            k=i.split(",")
+            self.list_of_call_objects.append(CallDetail(k[0],k[1],k[2],k[3]))
+        return self.list_of_call_objects
+
+call='9990000001,9330000001,23,STD'
+call2='9990000001,9330000002,54,Local'
+call3='9990000001,9330000003,6,ISD'
+
+list_of_call_string=[call,call2,call3]
+Util().parse_customer(list_of_call_string)
+print(Util().list_of_call_objects)
